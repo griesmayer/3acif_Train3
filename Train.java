a6a4b5f402096b0620ae8c252d76c8fc9f8d5e7f
Thomas Griesmayer <griesmayer@spengergasse.at>
2026-09-23 20:40:24 +0200

    Add getER

--- Train.java
+++ Train.java
@@ -3,4 +3,19 @@ public class Train
     private String  trainNumber;
     private int     seats;
     private boolean first;
+    
+    public String getTrainNumber()
+    {
+        return trainNumber;
+    }
+    
+    public int getSeats()
+    {
+        return seats;
+    }
+    
+    public boolean getFirst()
+    {
+        return first;
+    }    
 }
